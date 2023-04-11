0fa230bf73f136fd00ec624b8abebb5a7c6cdcdc
FlameOfTheFire1 <124635735+FlameOfTheFire1@users.noreply.github.com>
2023-04-10 23:07:08 -0700

    more functionality to create_event_rsvp

--- script/create_event_rsvp.js
+++ script/create_event_rsvp.js
@@ -1,8 +1,3 @@
-// document.querySelector() is used to select an element from the document using its ID
-//let QuestionText = document.querySelector('#previousQuestions');
-//var ctx = QuestionText.getContext("2d");
-//ctx.font = "30px Roboto";
-//ctx.fillStyle = "#08e5ff";
 
 let questionCounter = 2;
 let userText = document.querySelector('#questionBox');
@@ -12,37 +7,89 @@ let cancelButton = document.querySelector('#cancelFormButton');
 let saveQuestionButton = document.querySelector('#saveQuestionButton');
 let questionType = document.querySelector('#questionType');
 
-//var canvas = document.getElementById("previousQuestions");
-//var ctx = canvas.getContext("2d");
-//ctx.canvas.width  = window.innerWidth / 1.5;
-//ctx.canvas.height = window.innerHeight / 2;
-//ctx.font = "30px Arial";
-//ctx.fillText("After questions are saved they are added to this canvas", 10, 25);
-//ctx.globalCompositeOperation = 'destination-over'
-//ctx.fillStyle="white";
-//ctx.fillRect(0,0,canvas.width,canvas.height);
-
-
-
-
+///////////////////////////////////////////////////////////////////////////////////
+//when the user changes the option in the drop down menu, activate this function
 questionType.addEventListener('change', function(){
 
-console.log("a change has been made in the drop down menu");
+console.log("a change has been made in the drop down menu"); //when in doubt, print it out
+
+//get the input in the question box, question type selected from the drop down menu
 let input = document.getElementById("questionBox").value;
 let choice =  document.getElementById("questionType").value;
 
+//the number of custom options specified by the user starts at 0
+let customOptionCounter = 0;
+let customOptionID = "customOption"; //this is part of the id attribute of the custom option, will change slightly with every new option created
+let optionInputs = []; //array of all the input elements in the custom options
+
+//if the user has edited the questionboxe's value and the choices multichoice or checkboxes is selected
     if(input != "Enter your question" && (choice == "multiChoice" || choice == "checkBoxes")){
-        //use demo container to allow specifications
+        //use demo container shows the demo options
         console.log("multi choice or check boxes should be specified");
+        customOptionCounter++;//a new option counter is being generated, increment counter by 1
+        customOptionID = customOptionID + customOptionCounter; //generate a unique id for the custom option
 
-        html = "<p> " + input + ": </p>";
-        document.getElementById("questionDemo").innerHTML += html;
+        //html = "<p> " + input + ": </p>";
+        //document.getElementById("questionDemo").innerHTML += html;
 
-        html = "<input type='text' id='custom' name='custom' disabled><br><br>\n"
-        document.getElementById("questionDemo").innerHTML += html;
-    }
+        //this adds a check box (disabled) and a text field to specify the option
+        html = "<input type='checkbox' id='custom' name='custom' disabled>\n"
+            + "  <input type='text' id='" + customOptionID + "' name='custom' value = '' placeholder='option " + customOptionCounter +
+            "'>\n" + "<button id='addOptionButton' type='submit'>Add</button> \n" + //adds new option button
+            "<button id='removeOptionButton' type='submit'>Remove</button>\n"; //remove option button
+        document.getElementById("questionDemo").innerHTML += html; //insert the html into the document
+
+        //unfortunately, when a new option is generated, previous options are overwritten so we store them in an array
+        
+        //if(document.getElementById(customOptionID).value != ""){ //so we guard it until the option is added
+        //problematic, it doesnt wait for user input. . . 
+        console.log(customOptionID + " has been added");
+        optionInputs.push(document.getElementById(customOptionID).value); //push option text to the array
+        let nextOption = document.getElementById("addOptionButton"); //get the newly created button
+
+        //add a while statement to allow user to make options as long as they are not empty, have not hit save, clear, or . . .
+        //while((choice == "multiChoice" || choice == "checkBoxes") && customOptionCounter < 6)
+        //{
+        nextOption.addEventListener('click', function(){ //when the button is clicked, add a new option text box
+            console.log("runs");
+            console.log(document.getElementById(customOptionID).value);
+            //optionInputs.push(document.getElementById(customOptionID).value); //push option text to the array
+            optionInputs[customOptionCounter-1] = document.getElementById(customOptionID).value;
+            console.log(optionInputs[customOptionCounter-1]);
+
+
+            customOptionCounter++;// a new option counter is being generated, increment counter by 1
+            customOptionID = "customOption";
+            customOptionID = customOptionID + customOptionCounter; //generate a unique id for the new custom option
+            
+            html = "<br></br><input type='checkbox' id='custom' name='custom' disabled>\n"
+            + "  <input type='text' id='" + customOptionID + "' name='custom' value='' placeholder='option " + customOptionCounter +
+            "'>\n" + "<button id='addOptionButton' type='submit'>Add</button> \n" + //adds new option button
+            "<button id='removeOptionButton' type='submit'>Remove</button>\n"; //remove option button//generate the html for the new custom option and place it in the html element
+            document.getElementById("questionDemo").innerHTML += html;
+
+            customOptionID = "customOption"; //reset the custom option id
+            //loop puts all the option texts in the array back into the html elements
+            for(let i = 0; i < optionInputs.length; i++)
+            {
+                //console.log(optionInputs[i]);
+                customOptionID = customOptionID + (i + 1); //generate a unique id for the custom option corresponding to each index
+                console.log(customOptionID + " has been given text: " + optionInputs[i]);
+                document.getElementById(customOptionID).value = optionInputs[i]; //place the text in the array into the html element
+                customOptionID = "customOption"; //reset the custom option id
+            }
+        });
+    //}
+    } 
+
+    
 });
 
+doesNotExist.addEventListener('click', function(){
+    //do stuff
+});
+
+///////////////////////////////////////////////////////////////////////////////////////////////
 saveQuestionButton.addEventListener('click', function(){
 
     let input = document.getElementById("questionBox").value;
@@ -60,13 +107,13 @@ saveQuestionButton.addEventListener('click', function(){
 
         html = "<p> Question " + questionCounter + "--" + input + ": </p>";
         document.getElementById("questionContainer").innerHTML += html;
-
         switch(choice)
         {
-            case 'MultiChoice':
+            case 'multiChoice':
                 console.log("multi choice");//notthing yet
                 html = "<input type='text' id='custom' name='custom' disabled><br><br>\n"
                 document.getElementById("questionContainer").innerHTML += html;
+                document.getElementById("questionDemo").innerHTML = "";
                 break;
             case 'oneLine':
                 html = "<input type='text' id='custom' name='custom' disabled><br><br>\n"
@@ -77,6 +124,7 @@ saveQuestionButton.addEventListener('click', function(){
                 html = "<input type='checkbox' id='custom' name='custom' disabled><br><br>\n" + 
                 "<label for='option1'> option1 </label><br>\n"
                 document.getElementById("questionContainer").innerHTML += html;
+                document.getElementById("questionDemo").innerHTML = "";
                 break;
             case 'textBox':
                 html = "<textarea id='custom' name='custom' rows='4' cols='50' disabled></textarea><br><br>\n"
