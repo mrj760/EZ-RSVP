24871f6e3602bf5d6ca4b36ad7e3e6f09f7a710a
KB0129 <dlrudqs99@gmail.com>
2023-04-16 08:06:26 -0700

    user_dashboard.js event passing

--- script/user_dashboard.js
+++ script/user_dashboard.js
@@ -9,15 +9,33 @@ class Event {
     }
 }
 
-var event_node_array = new Array();
-event_node_array.push(new Event('','','','',''));
+// class that holds collection of events and properties and function for dashboard
+class Events {
+    constructor(){
+        this.names = [];
+        this.photoURLs = [];
+        this.details = [];
+        this.locations = [];
+        this.datetimes = [];
+    }
+    // create a new event and save it in the collection
+    newEvent(name, photoURL, detail, location, datetime){
+        let n = new Event(name)
+        let p = new Event(photoURL)
+        let d = new Event(detail)
+        let l = new Event(location)
+        let dt = new Event(datetime)
 
+        this.names.push(n)
+        this.photoURLs.push(p)
+        this.details.push(d)
+        this.locations.push(l)
+        this.datetimes.push(dt)
 
-
-window.addEventListener("load", function() {
-    displayEvent(event_node_array);
-});
-
-function displayEvent(){
+        return (n, p, d, l, dt)
+    }
+    get allEvents(){
+        return (this.names, this.photoURLs, this.details, this.locations, this.datetimes)
+    }
 
 }
