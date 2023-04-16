f758b51bd1a11967ed254668777bb30cf7d65d49
KB0129 <dlrudqs99@gmail.com>
2023-04-16 12:47:18 -0700

    change code

--- script/user_dashboard.js
+++ script/user_dashboard.js
@@ -9,79 +9,58 @@ class Event {
     }
 }
 
-// class that holds collection of events and properties and function for dashboard
-class dashEvents {
-    constructor(){
-        this.name_array = [];
-        this.photoURL_array = [];
-        this.detail_array = [];
-        this.location_array = [];
-        this.datetime_array = [];
-    }
-    // method: push the objects to array
-    dash_events(name, photoURL, detail, location, datetime){
-
-        // push the event to event array
-        this.name_array.push(name);
-        this.photoURL_array.push(photoURL);
-        this.detail_array.push(detail);
-        this.location_array.push(location);
-        this.datetime_array.push(datetime);
-
-    }
-    // get all events in event array
-    get allEvents(){
-        return (this.name_array, this.photoURL_array, this.detail_array, this.location_array, this.datetime_array);
-    }
-    // get length of event list
-    get numberOfEvents(){
-        return this.name_array.length;
-    }
-}
-
-
 //example: 
-let event_list = new dashEvents();
+let eventList = new Array();
 
-event_list.dash_events("Steve's BBQ", "https://~", "Eating BBQ with Steve", "San Marcos, CA", "October 4, 2023, 12:00pm");
-event_list.dash_events("Mike's Hunt", "https://~", "Hunting with Mike", "San Marcos, CA", "October 5, 2023, 12:00pm");
+eventList.push("Steve's BBQ", "https://~", "Eating BBQ with Steve", "San Marcos, CA", "October 4, 2023, 12:00pm");
+eventList.push("Mike's Hunt", "https://~", "Hunting with Mike", "San Marcos, CA", "October 5, 2023, 12:00pm");
 
 
 window.addEventListener("load", function() {
-    displayDashboardEvent(event_list);
+    displayDashboardEvent(eventList);
 });
 
 // In the dashboard, just show picture and name of event
 // show the event list using loop
 function displayDashboardEvent(event) {
-    for(let i=0; i<event_list.numberOfEvents; i++){
-        nameDiv = document.getElementById("dashboardEventName");
-        photoURLDiv = document.getElementById("dashboard_EventPic");
+    for(let i=0; i < eventList.length; i++){
         
-        var photo = document.createElement('img');
-        photo.src = event.photoURL_array[i];
+        let userEventsDiv = document.getElementById('userEventsDiv');
+
+        let eventDiv = document.createElement('div');
+        eventDiv.classList.add('event');
+        userEventsDiv.appendChild(eventDiv);
+        eventDiv.addEventListener('click', function () {
+            alert('you clicked on ' + event[i].name);
+        })
+        console.log(eventDiv);
+
+        let photoURLDiv = document.getElementById('photoURLDiv');
+
+        let photo = document.createElement('img');
+        photo.src = event[i].photoURL;
         photoURLDiv.appendChild(photo);
-
-        appendElmtToDiv(nameDiv, event.name_array[i]);
+        
+        appendElmtToDiv(userEventsDiv, eventList[i].name);
+        console.log(photoURLDiv);
     }
-}
 
+}
 function appendElmtToDiv(divNode, value, elmt='p') {
     let child = document.createElement(elmt);
     child.innerHTML = value;
     divNode.appendChild(child);
 }
+// // In the dashboardpage, you click the image or name of event
+// // then move to event details
+// window.onload = function(){
+//     var el = document.getElementById("dashboard_EventPic");
+//     var el_name = document.getElementById("dashboard_EventName");
 
-// In the dashboardpage, you click the image or name of event
-// then move to event details
-window.onload = function(){
-    var el = document.getElementById("dashboard_EventPic");
-    var el_name = document.getElementById("dashboard_EventName");
+//     el.onclick = clickEvent();
+//     el_name.onlcick = clickEvent();
+// }
 
-    el.onclick = click_event;
-    el_name.onlcick = click_event;
-}
-
-function click_event(){
-    alert('You click the event');
-}
+// function clickEvent(){
+//     alert('You click the event');
+// }
