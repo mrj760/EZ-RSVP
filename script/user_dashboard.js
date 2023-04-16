5ddeb56afdf648b578a2611fa3fb0c0af9300189
KB0129 <dlrudqs99@gmail.com>
2023-04-16 10:49:12 -0700

    fix class error

--- script/user_dashboard.js
+++ script/user_dashboard.js
@@ -10,44 +10,43 @@ class Event {
 }
 
 // class that holds collection of events and properties and function for dashboard
-class Events {
+class dashEvents {
     constructor(){
         this.name_array = [];
         this.photoURL_array = [];
         this.detail_array = [];
-        this.locations_array = [];
+        this.location_array = [];
         this.datetime_array = [];
     }
-    // create a new event and save it in the collection
-    newEvent(name, photoURL, detail, location, datetime){
-        let n = new Event(name)
-        let p = new Event(photoURL)
-        let d = new Event(detail)
-        let l = new Event(location)
-        let dt = new Event(datetime)
+    // method: push the objects to array
+    dash_events(name, photoURL, detail, location, datetime){
 
         // push the event to event array
-        this.name_array.push(n)
-        this.photoURL_array.push(p)
-        this.detail_array.push(d)
-        this.location_array.push(l)
-        this.datetime_array.push(dt)
+        this.name_array.push(name);
+        this.photoURL_array.push(photoURL);
+        this.detail_array.push(detail);
+        this.location_array.push(location);
+        this.datetime_array.push(datetime);
 
-        return (n, p, d, l, dt)
     }
     // get all events in event array
     get allEvents(){
-        return (this.names, this.photoURLs, this.details, this.locations, this.datetimes)
+        return (this.name_array, this.photoURL_array, this.detail_array, this.location_array, this.datetime_array);
+    }
+    // get length of event list
+    get numberOfEvents(){
+        return this.name_array.length;
     }
 }
 
 
 //example: 
-let event_list = new Events();
+let event_list = new dashEvents();
 
-event_List.newEvent("Steve's BBQ", "https://~", "Eating BBQ with Steve", "San Marcos, CA", "October 4, 2023, 12:00pm" )
-event_List.newEvent("Mike's Hunt", "https://~", "Hunting with Mike", "San Marcos, CA", "October 5, 2023, 12:00pm" )
+event_list.dash_events("Steve's BBQ", "https://~", "Eating BBQ with Steve", "San Marcos, CA", "October 4, 2023, 12:00pm");
+event_list.dash_events("Mike's Hunt", "https://~", "Hunting with Mike", "San Marcos, CA", "October 5, 2023, 12:00pm");
 
+console.log(dashEvents.allEvents);
 
 window.addEventListener("load", function() {
     displayDashboardEvent(event_list);
@@ -56,12 +55,15 @@ window.addEventListener("load", function() {
 // In the dashboard, just show picture and name of event
 // show the event list using loop
 function displayDashboardEvent(event) {
-    for(let i=0; i<Events.names.length; i++){
+    for(let i=0; i<event_list.numberOfEvents; i++){
         nameDiv = document.getElementById("dashboardEventName");
-        photoURLDiv = document.getElementById("dashboardEventPic");
+        photoURLDiv = document.getElementById("dashboard_EventPic");
         
-        appendElmtToDiv(nameDiv, event.names[i])
-        appendElmtToDiv(photoURLDiv, event.photoURLs[i])
+        var photo = document.createElement('img');
+        photo.src = event.photoURL_array[i];
+        photoURLDiv.appendChild(photo);
+
+        appendElmtToDiv(nameDiv, event.name_array[i]);
     }
 }
 
@@ -69,4 +71,18 @@ function appendElmtToDiv(divNode, value, elmt='p') {
     let child = document.createElement(elmt);
     child.innerHTML = value;
     divNode.appendChild(child);
+}
+
+// In the dashboardpage, you click the image or name of event
+// then move to event details
+window.onload = function(){
+    var el = document.getElementById("dashboard_EventPic");
+    var el_name = document.getElementById("dashboard_EventName");
+
+    el.onclick = click_event;
+    el_name.onlcick = click_event;
+}
+
+function click_event(){
+    alert('You click the event');
 }
