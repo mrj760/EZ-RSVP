9f48e7ad2c561464697bf4cbe88faa476fd818ba
FlameOfTheFire1 <124635735+FlameOfTheFire1@users.noreply.github.com>
2023-05-04 00:21:44 -0700

    Update about.php

--- pages/about.php
+++ pages/about.php
@@ -11,7 +11,7 @@ and links to create an account or login. -->
 
 <body>
     <p>Create your dream event in minutes</P>
-    <img src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ibtimes.co.uk%2Fprince-georges-first-birthday-legoland-create-lego-buckingham-palace-party-scene-1457682&psig=AOvVaw0X1HyElo1Pm7s6zo4JK7Hw&ust=1683266312026000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCNj8hOf92v4CFQAAAAAdAAAAABAN">
+    <img src = "https://cdn.pixabay.com/photo/2017/07/21/23/57/concert-2527495_1280.jpg">
     <img src = "https://images.app.goo.gl/DfEt1stwKLXqy3TW6">
     <p>Invite attendees to rsvp with a link</P>
     <img src = "https://images.app.goo.gl/B8tvwdEyFgRJDYcBA">
