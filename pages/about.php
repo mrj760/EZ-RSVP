2d2aeca0c5692d91d1308407cc6b5caceb83987d
FlameOfTheFire1 <124635735+FlameOfTheFire1@users.noreply.github.com>
2023-05-03 23:09:33 -0700

    Update about.php

--- pages/about.php
+++ pages/about.php
@@ -10,6 +10,12 @@ and links to create an account or login. -->
 </head>
 
 <body>
+    <p>Create your dream event in minutes</P>
+    <img src = "https://images.app.goo.gl/KoN6k6oLYe3qEJYC6">
+    <img src = "https://images.app.goo.gl/DfEt1stwKLXqy3TW6">
+    <p>Invite attendees to rsvp with a link</P>
+    <img src = "https://images.app.goo.gl/B8tvwdEyFgRJDYcBA">
+    <p>Collect responses from attendees with rsvp form</P>
 
 </body>
 
