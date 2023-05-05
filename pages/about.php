7e360f0cf1791abaef6f706770009c0c30613da0
FlameOfTheFire1 <124635735+FlameOfTheFire1@users.noreply.github.com>
2023-05-04 19:13:17 -0700

    about

--- pages/about.php
+++ pages/about.php
@@ -11,12 +11,14 @@ and links to create an account or login. -->
 
 <body>
 
+    <div id = "text">
     <p>EZ-RSVP aims to make your event run more smoothly by tracking reservations, guest logistics, and more! </p>
     <p>1. Create an account.</P>
-    <p>2. Create your dream event in minutes</P>
-    <p>3. Invite attendees to rsvp with a link</P>
-    <p>4. Collect responses from attendees with rsvp form</P>
-    <img src = "https://thumbnails.production.thenounproject.com/7lYDhxRfPTPpF9SpchnvqrGStyo=/fit-in/1000x1000/photos.production.thenounproject.com/photos/C30D2809-9294-4B11-A2AE-A405A00A9893.jpg">
+    <p>2. Create your dream event in minutes.</P>
+    <p>3. Invite attendees to rsvp with a link.</P>
+    <p>4. Collect responses from attendees with rsvp form.</P>
+    </div>
+    <img id = "img" src = "https://thumbnails.production.thenounproject.com/7lYDhxRfPTPpF9SpchnvqrGStyo=/fit-in/1000x1000/photos.production.thenounproject.com/photos/C30D2809-9294-4B11-A2AE-A405A00A9893.jpg">
 </body>
 
 </html>
