c5d438230c888344a33bf4cae045fa5169164e44
KB0129 <dlrudqls99@gmail.com>
2023-05-06 21:30:36 -0700

    revise logout

--- script/navbar.js
+++ script/navbar.js
@@ -23,7 +23,7 @@ navDiv.innerHTML =
     "            <a href=\"user_login.php\" class=\"nav-link\">\n" +
     "               <i class=\"fa-solid fa-right-to-bracket\"></i> Login" + "<?php } ?>" +
     "           <?php else {" + "<a href=\"user_login.php\" class=\"nav-link\">\n" + 
-    "               <i class=\"fa-solid fa-right-to-bracket\"></i> Logout" + "<?php } ?>"
+    "               <i class=\"fa-solid fa-right-to-bracket\"></i> Logout" + "<?php } ?>" +
     "            </a>\n" + 
     "        </li>\n" +
     "        <li class=\"nav-item\">\n" +
