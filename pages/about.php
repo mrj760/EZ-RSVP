f55c6d16463e2109e31e4961b50c444169700396
FlameOfTheFire1 <124635735+FlameOfTheFire1@users.noreply.github.com>
2023-05-08 21:40:03 -0700

    about

--- pages/about.php
+++ pages/about.php
@@ -11,23 +11,24 @@ and links to create an account or login. -->
 
 <body>
 
-
+<div class = "container">
     <div class = "text">
     <p>Welcome to EZ-RSVP! EZ-RSVP is an event reservation website that aims to make your event planning go smoothly.
 
-Whether you're planning a small gathering with friends or a corporate conference, you can easily invite people you know
-with a link.  With a user friendly interface, you can easily keep track of reservations, collect important information 
-through a questionare, and host your event publicly on the website.
+    Whether you're planning a small gathering with friends or a corporate conference, you can easily invite people you know
+    with a link.  With a user friendly interface, you can easily keep track of reservations, collect important information 
+    through a questionare, and host your event publicly on the website.
 
-We understand that planning an event can be overwhelming, which is why we've designed our website to provide all the 
-logistics you need in one place. From detailed event descriptions and photos to questionares asked with every
-reservation, our site offers everything you need to take your event to the next level. 
-</p>
-
+    We understand that planning an event can be overwhelming, which is why we've designed our website to provide all the 
+    logistics you need in one place. From detailed event descriptions and photos to questionares asked with every
+    reservation, our site offers everything you need to take your event to the next level. 
+    </p>
     </div>
-    <div class = "image">
     
+    <div class = "image">
+    <img src = "https://thumbnails.production.thenounproject.com/7lYDhxRfPTPpF9SpchnvqrGStyo=/fit-in/1000x1000/photos.production.thenounproject.com/photos/C30D2809-9294-4B11-A2AE-A405A00A9893.jpg">
     </div>
+</div>
 </body>
 
 </html>
