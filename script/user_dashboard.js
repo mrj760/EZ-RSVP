fd968b733a78368472ade83631496724ac2b4984
Chris Ledet <83104282+cj-ledet@users.noreply.github.com>
2023-05-08 18:54:19 -0700

    Testing dashboard

--- script/user_dashboard.js
+++ script/user_dashboard.js
@@ -1,14 +1,14 @@
 import { getCookie, setCookie } from "./cookie_functions";
+import { Event } from "./event_object";
 
 window.addEventListener("load", function () {
-    displayDashboardEvent(eventList);
+    displayDashboardEvent();
 });
 
 // In the dashboard, just show picture and name of event
 // show the event list using loop
 function displayDashboardEvent() {
 
-    console.log(getCookie('events'));
     let userEventsDiv = document.getElementById('userEventsDiv');
 
     let events = localStorage.getItem('events');
@@ -16,27 +16,23 @@ function displayDashboardEvent() {
     for (let i = 0; i < events.length; i++) {
 
         console.log(events[i]);
-
+        
         let linkToEventDetails = document.createElement('a');
         linkToEventDetails.classList.add('userEvent');
         linkToEventDetails.href = "./event_details.php";
 
         let eventDiv = document.createElement('div');
-        eventDiv.addEventListener('click', function () {
-            setCookie
-        });
-
         let photoDiv = document.createElement('div');
         photoDiv.classList.add('photoDiv');
         let photo = document.createElement('img');
-        photo.src = eventList[i].photoURL;
+        photo.src = events[i].photoURL;
         photoDiv.appendChild(photo);
         eventDiv.appendChild(photoDiv);
 
         let eventNameDiv = document.createElement('div');
         eventNameDiv.classList.add('eventNameDiv');
         let eventName = document.createElement('p');
-        eventName.innerHTML = eventList[i].name;
+        eventName.innerHTML = events[i].name;
         eventName.classList.add('eventName');
         eventNameDiv.appendChild(eventName);
         eventDiv.appendChild(eventNameDiv);
