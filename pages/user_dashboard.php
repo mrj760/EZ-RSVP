a62fc1d9d860a5c2edefbafd082857262b2ab062
Chris Ledet <83104282+cj-ledet@users.noreply.github.com>
2023-05-08 19:59:28 -0700

    Adding JSON stringify

--- pages/user_dashboard.php
+++ pages/user_dashboard.php
@@ -31,7 +31,7 @@
     ?>
     <script type="text/javascript">
         let events = <?= json_encode($events)?>;
-        localStorage.setItem('events', events);
+        localStorage.setItem('events', JSON.stringify(events));
     </script>
     <br>
     <h1 id="greeting"><?= 'Hello ' . $_COOKIE['username'] . '!' ?></h1>
