36f60eb293b8ec501ccd4ffe09eaea7bd246b22c
Chris Ledet <83104282+cj-ledet@users.noreply.github.com>
2023-05-08 19:55:04 -0700

    Testing localStorage fix

--- pages/user_dashboard.php
+++ pages/user_dashboard.php
@@ -31,7 +31,7 @@
     ?>
     <script type="text/javascript">
         let events = <?= json_encode($events)?>;
-        localStorage.setCookie('events', events, 2);
+        localStorage.setItem('events', events, 2);
     </script>
     <br>
     <h1 id="greeting"><?= 'Hello ' . $_COOKIE['username'] . '!' ?></h1>
