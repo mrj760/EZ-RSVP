389ac9006ddb5a77fd92ed88fd5e1c7f645b13f4
KB0129 <dlrudqls99@gmail.com>
2023-05-10 21:01:27 -0700

    fix syntax

--- pages/rsvp.php
+++ pages/rsvp.php
@@ -25,7 +25,7 @@
             if (isset($_POST['name']) && isset($_POST['email'])){
 
                $GUEST = array(
-                $_GET['eventID'],
+                $_GET['id'],
                 $_POST['name'],
                 $_POST['email']
                );
