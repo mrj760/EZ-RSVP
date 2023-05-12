62a24d972b1355873cf9cf0e54eb980b22666ac5
Chris Ledet <83104282+cj-ledet@users.noreply.github.com>
2023-05-11 17:39:53 -0700

    testing possible fix

--- script/create_event_rsvp.js
+++ script/create_event_rsvp.js
@@ -41,6 +41,11 @@ class Question {
         this.text.type = "text";
         this.text.name = 'question' + questionNumber;
 
+        this.qType = document.createElement('input');
+        this.qType.type = 'text';
+        this.qType.name = 'question' + questionNumber + '-type';
+        this.qType.hidden = true;
+        
         this.select = document.createElement("select");
         this.offerQuestionType('text', 'Text');
         this.offerQuestionType('singlepick', 'Single Pick');
@@ -54,19 +59,15 @@ class Question {
         this.answerOptions = [];
         this.select.addEventListener("change", function (e) {
             me.type = e.currentTarget.value;
+            this.type = me.type;
+            this.qType.value = this.type;
             me.fillOptions();
         });
         this.newOptionButton.addEventListener("click", function () {
             let optionNum = me.answerOptions.length + 1;
             me.answerOptions.push(new AnswerOption(me.type, questionNumber, optionNum));
             me.fillOptions();
-        })
-        
-        this.qType = document.createElement('input');
-        this.qType.type = 'text';
-        this.qType.name = 'question' + questionNumber + '-type';
-        this.qType.value = 'test';
-        this.qType.hidden = true;
+        })        
 
         this.leftDiv.appendChild(this.label);
         this.leftDiv.appendChild(document.createElement('br'));
